d9bc214247d36d369777a0587c480071d83c343a
Iordan <kiordan@yandex.ru>
2022-12-11 18:27:45 +0300

    feat: написать скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -6,30 +6,32 @@ let username = document.querySelector('.profile__username');
 let userRole = document.querySelector('.profile__user-role');
 let newName = document.querySelector('.form__input-text_type_name');
 let newRole = document.querySelector('.form__input-text_type_role');
-let alertMessage = document.querySelector('.form__alert');
-
-editButton.addEventListener('click', open_popup);
-closeButton.addEventListener('click', close_popup);
-form.addEventListener('submit', update_profile);
+const likes = document.querySelectorAll('.places__like');
 
 function open_popup() {
-  popup.classList.add('popup_showed');
   newName.value = username.textContent;
   newRole.value = userRole.textContent;
+  popup.classList.add('popup_showed');
 }
 
 function close_popup() {
   popup.classList.remove('popup_showed');
 }
 
-function show_alert() {
-  alertMessage.classList.add('popup_showed');
-}
-
-show_alert();
-
 function update_profile(evt) {
   evt.preventDefault();
   username.textContent = newName.value;
   userRole.textContent = newRole.value;
+  close_popup()
+}
+
+editButton.addEventListener('click', open_popup);
+closeButton.addEventListener('click', close_popup);
+form.addEventListener('submit', update_profile);
+
+
+for (let like of likes) {
+  like.addEventListener('click', function() {
+    like.classList.toggle('places__like_liked');
+  })
 }
