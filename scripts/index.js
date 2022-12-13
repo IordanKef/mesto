ccae5bff09514408dead1469049528cf2d9f437f
Iordan <kiordan@yandex.ru>
2022-12-13 16:41:30 +0300

    fix: внести правки ревью

--- scripts/index.js
+++ scripts/index.js
@@ -6,32 +6,24 @@ let username = document.querySelector('.profile__username');
 let userRole = document.querySelector('.profile__user-role');
 let newName = document.querySelector('.form__input-text_type_name');
 let newRole = document.querySelector('.form__input-text_type_role');
-const likes = document.querySelectorAll('.places__like');
 
-function open_popup() {
+function openPopup() {
   newName.value = username.textContent;
   newRole.value = userRole.textContent;
   popup.classList.add('popup_showed');
 }
 
-function close_popup() {
+function closePopup() {
   popup.classList.remove('popup_showed');
 }
 
-function update_profile(evt) {
+function updateProfile(evt) {
   evt.preventDefault();
   username.textContent = newName.value;
   userRole.textContent = newRole.value;
-  close_popup()
+  closePopup()
 }
 
-editButton.addEventListener('click', open_popup);
-closeButton.addEventListener('click', close_popup);
-form.addEventListener('submit', update_profile);
-
-
-for (let like of likes) {
-  like.addEventListener('click', function() {
-    like.classList.toggle('places__like_liked');
-  })
-}
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', updateProfile);
